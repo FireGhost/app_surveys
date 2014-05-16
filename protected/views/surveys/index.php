4abea7ce55cf9ac9d58bbf911455c1b99dddf88e
PouleRequest <poulerequest@gmail.com>
2014-05-16 10:46:57 +0200

    Fixed some weird stuff after the last merge

--- protected/views/surveys/index.php
+++ protected/views/surveys/index.php
@@ -1,5 +1,5 @@
 <?php 
-echo CHtml::link("Create new", 'surveys/create');
+echo CHtml::link("Create new", 'create');
 $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view'
