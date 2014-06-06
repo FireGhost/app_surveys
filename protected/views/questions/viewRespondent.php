d8206d73b0886df58884f84266471d792c96e3ec
Allan Mermod <allan.mermod@bluemail.ch>
2014-06-06 10:25:49 +0200

    Added the verification if the taking is locked (still can see the taking but edition is impossible).

--- protected/views/questions/viewRespondent.php
+++ protected/views/questions/viewRespondent.php
@@ -1,21 +1,29 @@
 <div class="view">
 <?php
+    $isLocked = $this->isLocked($_GET['id']);
+    
     echo '<b>'. CHtml::encode($question->title) .'</b><br />';
     
     switch ($question->type) {
+        
+        
         case 'UniqueChoiceQuestion':
             foreach ($question->propositions as $proposition) {
-                echo CHtml::radioButton('Questions['. $question->id .']', false, array('value' => $proposition->id));
+                echo CHtml::radioButton('Questions['. $question->id .']', false, array('value' => $proposition->id, 'disabled' => ($isLocked ? 'disabled':'') ));
                 echo ' '. CController::renderPartial("//propositions/viewRespondent", array('proposition'=>$proposition)) .'<br />';
             }
         break;
         
+        
+        
         case 'MultipleChoiceQuestion':
             foreach ($question->propositions as $proposition) {
-                echo CHtml::checkBox('Questions['. $question->id .']['. $proposition->id .']', false, array('value' => $proposition->id));
+                echo CHtml::checkBox('Questions['. $question->id .']['. $proposition->id .']', false, array('value' => $proposition->id, 'disabled' => ($isLocked ? 'disabled':'')));
                 echo ' '. CController::renderPartial("//propositions/viewRespondent", array('proposition'=>$proposition)) .'<br />';
             }
         break;
+        
+        
         
         case 'RangeQuestion':
             
@@ -33,24 +41,28 @@
                 echo CController::renderPartial("//propositions/viewRespondent", array('proposition'=>$proposition));
                 echo '<div id="range_'. $proposition->id .'" style="width: 300px; margin-top: 3px;"></div>';
                 echo '<div class="val_'. $proposition->id .'" style="margin-bottom: 2px;">'. $settings->min .'</div>';
-                echo '<input type="hidden" name="Questions['. $question->id .']['. $proposition->id .']" class="val_'. $proposition->id .'" value="'. $settings->min .'"/>';
-                
-                
-                $this->widget('zii.widgets.jui.CJuiSlider', array(
-                    'value'=>intval($settings->min),
-                    'id'=>'range_'. $proposition->id,
-                    'options'=>array(
-                        'min' => intval($settings->min),
-                        'max' => intval($settings->max),
-                        'step' => intval($settings->step),
-                        'slide'=>'js:function(event, ui) {
-                            $("div.val_'. $proposition->id .'").html(ui.value);
-                            $("input.val_'. $proposition->id .'").val(ui.value);
-                        }'
-                    )
-                ));
+                if (!$isLocked) {
+                    echo '<input type="hidden" name="Questions['. $question->id .']['. $proposition->id .']" class="val_'. $proposition->id .'" value="'. $settings->min .'"/>';
+                    
+                    
+                    $this->widget('zii.widgets.jui.CJuiSlider', array(
+                        'value'=>intval($settings->min),
+                        'id'=>'range_'. $proposition->id,
+                        'options'=>array(
+                            'min' => intval($settings->min),
+                            'max' => intval($settings->max),
+                            'step' => intval($settings->step),
+                            'slide'=>'js:function(event, ui) {
+                                $("div.val_'. $proposition->id .'").html(ui.value);
+                                $("input.val_'. $proposition->id .'").val(ui.value);
+                            }'
+                        )
+                    ));
+                }
             }
         break;
+        
+        
         
         case 'RankingQuestion':
             
@@ -64,18 +76,27 @@
             echo '</div>';
             
             if (!empty($propositionsItems)) {
-                $this->widget('zii.widgets.jui.CJuiSortable',array(
-                    'items' => $propositionsItems,
-                    'id' => 'question_'.  $question->id,
-                    'itemTemplate' => '<li id="{id}">{content}</li>',
-                    'options' => array(
-                        'update' => 'js:function(event, ui) {
-                            $(this).find("li").each( function(i) {
-                                $("div#rank_'. $question->id .'").find("input[name=\'Questions['. $question->id .']["+ $(this).attr("id") +"]\']").val(i+1);
-                            });
-                        }'
-                    )
-                ));
+                if (!$isLocked) {
+                    $this->widget('zii.widgets.jui.CJuiSortable',array(
+                        'items' => $propositionsItems,
+                        'id' => 'question_'.  $question->id,
+                        'itemTemplate' => '<li id="{id}">{content}</li>',
+                        'options' => array(
+                            'update' => 'js:function(event, ui) {
+                                $(this).find("li").each( function(i) {
+                                    $("div#rank_'. $question->id .'").find("input[name=\'Questions['. $question->id .']["+ $(this).attr("id") +"]\']").val(i+1);
+                                });
+                            }'
+                        )
+                    ));
+                }
+                else {
+                    echo '<ul>';
+                    foreach($propositionsItems as $propositionItem) {
+                        echo '<li>'. $propositionItem .'</li>';
+                    }
+                    echo '</ul>';
+                }
             }
             
         break;
--- protected/views/takings/_view.php
+++ protected/views/takings/_view.php
@@ -3,11 +3,12 @@
 /* @var $data Taking */
 ?>
 
-
-<div class="view">
+<?php $locked = $this->isLocked($data->id); ?>
+<div class="view <?php echo ($locked ? 'locked-element' : '');?>">
     
     <?php echo CHtml::link("<h4>".CHtml::encode($data->survey->title)."</h4>", array('view', 'id'=>$data->id), array('title' => $data->comment)); ?>
-    <?php echo CHtml::encode($data->survey->description); ?>
+    <?php echo CHtml::encode($data->survey->description); ?><br />
+    <?php echo 'Comment: '. CHtml::encode($data->comment); ?>
     
 </div>
 
