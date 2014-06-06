0e107d5ac2f5b8305a050bca4dc2496ac5065ddf
PouleRequest <poulerequest@gmail.com>
2014-06-06 10:03:30 +0200

    Corrected bug in ActiveResource

--- protected/models/IntranetUser.php
+++ protected/models/IntranetUser.php
@@ -2,8 +2,8 @@
 //require_once ('ActiveResource.php');
 
 class IntranetUser extends ActiveResource {
-	var $element_name = 'person';
-	var $collection_name = 'people';
+	var $element_name = 'user';
+	var $collection_name = 'users';
 	
 }
 ?>
